f07ac685979a084109253ca6b7758067809bb2e5
SHADOW_MD 🔰 <mdshadow692@gmail.com>
2024-08-18 18:49:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const port = process.env.PORT || 8000;
 //=============================================
 
 async function connectToWA() {
-console.log("Connecting SHADOW-MD_🤖🔰");
+console.log("Connecting SHADOW-MD🔰");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
 
