bedcb4d974e57af7f87a10be6233bc16c8e49f6c
SHADOW_MD 🔰 <mdshadow692@gmail.com>
2024-08-19 08:27:10 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -8,7 +8,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-reply("*🔰 welcome to SHADOW-MD menu🔰*\n\n|💫+++++Commands+++++💫|\n\n*💻 Command - .alive*\n\n*✅ බොට් ඔන්ලයින්ද බැලීම*\n\n*💻 Command - .menu*\n\n*✅ බොට්ගෙ විධාන මෙනුව ලබාගැනීම*\n\n*💻 Command - .song*\n\n*✅ සිංදු බාගත කිරීම*\n\n*💻 Command - .video*\n\n*✅ වීඩියෝ බාගත කිරීම*\n\n🔰▁SHADOW-MD by lakshan damayantha▁▁▁▁▁🔰")}catch(e){
+reply("*🔰 welcome to SHADOW-MD menu🔰*\n\n|💫+++++Commands+++++💫|\n\n*💻 Command - .alive*\n\n*✅ බොට් ඔන්ලයින්ද බැලීම*\n\n*💻 Command - .menu*\n\n*✅ බොට්ගෙ විධාන මෙනුව ලබාගැනීම*\n\n*💻 Command - .song*\n\n*✅ සිංදු බාගත කිරීම*\n\n*💻 Command - .video*\n\n*✅ වීඩියෝ බාගත කිරීම*\n\n*.coomend- .ai💻*\n\n*✅ai තාක්ශනය බාවිතය*\n\n*🔰▁SHADOW-MD by lakshan damayantha▁▁▁▁▁🔰*")}catch(e){
 console.log(e)
 reply(`${e}`)
 }
