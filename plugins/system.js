edc6cc155c2461e145ad0796811ab64ace242709
SHADOW_MD 🔰 <mdshadow692@gmail.com>
2024-08-19 17:06:53 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -11,10 +11,10 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `*Uptime:*  ${runtime(process.uptime())}
-*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-*HostName:* ${os.hostname()}
-*Owner:* Prabath Kumara & shadow-md
+let status = `*📟👑 Uptime:*  ${runtime(process.uptime())}
+*📟💻 Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*🔎🏷️ HostName:* ${os.hostname()}
+*👨‍💻🖇️ Owner:* Prabath Kumara & shadow-md
 `
 return reply(`${status}`)
 }catch(e){
