ec9f62c6ff66e3daeb2b9c3452a4284626946219
SHADOW_MD 🔰 <mdshadow692@gmail.com>
2024-08-24 20:27:27 +0530

    Create ping.js

---
+++ plugins/ping.js
@@ -0,0 +1,27 @@
+cmd({
+        pattern: "ping",
+        react: "📟",
+        alias: ["speed"],
+        desc: "Check bot\'s ping",
+        category: "other",
+        use: '.ping',
+        filename: __filename
+    },
+    async (conn, mek, m, {
+        from,
+        reply
+    }) => {
+        try {
+            let inital = new Date().getTime();
+            let ping = await conn.sendMessage(from, {
+                text: '```Pinging To index.js!!!```'
+            }, {
+                quoted: mek
+            })
+            let final = new Date().getTime();
+            return await conn.edit(ping, '*Pong*\n *' + (final - inital) + ' ms* ')
+        } catch (e) {
+            reply('*Error !!*')
+            console.log(e)
+        }
+    })
