49bac4ef53f012527083581e69a934e37ae9b5e4
SHADOW_MD 🔰 <mdshadow692@gmail.com>
2024-08-25 11:07:31 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -11,7 +11,7 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
 let des = `OWENER NAMBAR 94767910958👨‍💻💗📍`
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: des},{quoted: mek})
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/52720ef1ec768bc4f45b7.jpg`},caption:dec},{quoted:mek});
 }catch(e){
 console.log(e)
 reply(`${e}`)
