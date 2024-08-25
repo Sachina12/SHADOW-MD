4a7a29f8cf50306a9a8733e4700ba597e5db5ab2
SHADOW_MD 🔰 <mdshadow692@gmail.com>
2024-08-25 11:00:07 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -11,7 +11,7 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         const startTime = Date.now()
-        const message = await conn.sendMessage(from, { text: '𝗣𝗶𝗻𝗴𝗶𝗻𝗴...' })
+        const message = await conn.sendMessage(from, { text: 'waite..' })
         const endTime = Date.now()
         const ping = endTime - startTime
         await conn.sendMessage(from, { text: `OWNER NAMBAR 94767910958 : ${owener}` }, { quoted: message })
