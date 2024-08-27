0d553ce9838c5055b70a0f839ede8a28e1feec12
QUEEN KYLIE - MD🔰 <mdshadow692@gmail.com>
2024-08-27 12:36:19 +0530

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -10,7 +10,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let des = `👾 Hi SHADOW-MD🔰\n\n*🌈 Owner* - SHADOW-MD - Lakshan damayantha\n\n*Repo Link:https://github.com/vipshadowmd/SHADOW-MD👾💯\n\n*`
+let des = `👾 Hi SHADOW-MD🔰\n\n*🌈 Owner* - SHADOW-MD - Lakshan damayantha\n\n*Repo Link:https://github.com/vipshadowmd/SHADOW-MD👾💯*> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝙻𝙰𝙺𝚂𝙷𝙰𝙽 𝚝𝚎𝚊𝚌𝚑*\n\n`
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: des},{quoted: mek})
 }catch(e){
 console.log(e)
