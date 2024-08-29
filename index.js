8e594ecb574012c2ceea4bc326bb66775dcb7160
Dark Yasiya <152196948+DarkYasiyaofc@users.noreply.github.com>
2024-08-29 03:26:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -144,6 +144,14 @@ if(senderNumber.includes("94767910958")){
 if(isReact) return
 m.react("👑")
 }
+        
+if (config.AUTO_VOICE === 'true') {
+const url = 'https://raw.githubusercontent.com/DarkYasiyaofc/VOICE/main/Voice-Raw/FROZEN-V2?token=GHSAT0AAAAAACWWWHYNOS7M5CMF34JGXVVGZWQJT2A'
+let { data } = await axios.get(url)
+for (vr in data){
+if((new RegExp(`\\b${vr}\\b`,'gi')).test(body)) conn.sendMessage(from,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:mek})   
+ }}
+
 
 const events = require('./command')
 const cmdName = isCmd ? body.slice(1).trim().split(" ")[0].toLowerCase() : false;
