3366988b3eff2b4c98f066971758905b4eb4b24a
Dark Yasiya <152196948+DarkYasiyaofc@users.noreply.github.com>
2024-08-29 03:32:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ m.react("👑")
 }
         
 if (config.AUTO_VOICE === 'true') {
-const url = 'https://raw.githubusercontent.com/DarkYasiyaofc/VOICE/main/Voice-Raw/FROZEN-V2?token=GHSAT0AAAAAACWWWHYNOS7M5CMF34JGXVVGZWQJT2A'
+const url = 'https://raw.githubusercontent.com/DarkYasiyaofc/VOICE/main/Voice-Raw/FROZEN-V2'
 let { data } = await axios.get(url)
 for (vr in data){
 if((new RegExp(`\\b${vr}\\b`,'gi')).test(body)) conn.sendMessage(from,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:mek})   
