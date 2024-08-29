cc7f4c76c20d8509144563c66266fd2b3ab4d8df
Dark Yasiya <152196948+DarkYasiyaofc@users.noreply.github.com>
2024-08-29 03:27:12 -0700

    Update config.js

--- config.js
+++ config.js
@@ -10,4 +10,5 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/357b75726739a8b9c68
 SUDO_NB: process.env.SUDO_NB || "ඔයාගෙ owner number එක දෙන්න",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
+AUTO_VOICE: "true"
 };
