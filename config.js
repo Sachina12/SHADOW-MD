5ab28c9615e2dfdb746cfebd124096b87d57c42d
QUEEN KYLIE - MD🔰 <mdshadow692@gmail.com>
2024-08-29 10:01:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "put sesson id",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/ecc50749e60a73fc28ce0.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi SHADOW-MD Is Online Now 💻\n*💻 Owner* - SHADOW MD\n\n*💻 Owner Number* -94767910958\n\n_විධාන මෙනුව ලබා ගැනීමට .menu ලෙස ටයිප් කරන්න._",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/357b75726739a8b9c68de.jpg",
 SUDO_NB: process.env.SUDO_NB || "ඔයාගෙ owner number එක දෙන්න",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
